f885d5cb276c4e20e9d69b29d6ef9505ffa73112
DevEnrqiue <luiscuyap@gmail.com>
2025-06-09 21:38:57 -0500

    feat: Se instala paquetes en class library Shared y se crea catalogModule en class library Catalogo

--- src/Bootstrapper/Api/Program.cs
+++ src/Bootstrapper/Api/Program.cs
@@ -1,5 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 
+//Add services to the container
+builder.Services
+    .AddCatalogoModule(builder.Configuration)
+    .AddCarritoModule(builder.Configuration)
+    .AddPedidoModule(builder.Configuration);
+
 var app = builder.Build();
 
 app.Run();
---
+++ src/Modules/Catalogo/Catalogo/CatalogoModule.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Catalogo;
+public static class CatalogoModule
+{
+    public static IServiceCollection AddCatalogoModule(this IServiceCollection services, IConfiguration configuration)
+    {
+        // Register services, repositories, etc. here
+        // Example: services.AddScoped<ICatalogService, CatalogService>();
+        return services;
+    }
+}
