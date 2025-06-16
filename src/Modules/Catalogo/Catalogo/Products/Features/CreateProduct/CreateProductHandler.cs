0cf554533c8754588f087d633c76604dfc972086
Luis Cuya <luis.cuya@intelica.com>
2025-06-16 15:36:30 -0500

    006 - Se agrega endpoint part I CrearProducto con CQRS

---
+++ src/Modules/Catalogo/Catalogo/Products/Features/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+
+namespace Catalogo.Products.CreateProduct;
+
+//lo que se necesita para crear un producto
+public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price) : IRequest<CreateProductResult>;
+
+//lo que se devuelve al crear un producto
+public record CreateProductResult(Guid id);
+
+internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+{
+    public Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    {
+        // Aquí iría la lógica para crear el producto
+        // Por ahora, devolvemos un ID ficticio
+        var productId = Guid.NewGuid();
+        return Task.FromResult(new CreateProductResult(productId));
+    }
+}
+
+
