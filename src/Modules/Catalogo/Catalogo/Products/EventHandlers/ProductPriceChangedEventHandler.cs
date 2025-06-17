804f5f5b4ccd50450b3c08be311d54930eb15475
Luis Cuya <luis.cuya@intelica.com>
2025-06-17 13:53:59 -0500

    006 - ProductPriceChangedEventHandler: Este manejador de eventos se dispara cuando un product es actualizado con exito y se modifica el precio de un producto. Funcion: registra la informacion que indica que el evento de dominio de updateProduct ha sido manejado, y el registrador captura el tipo de evento y este lo que hara es un evento de integracion para comunicarse con el modulo de Carrito usando rabbit

---
+++ src/Modules/Catalogo/Catalogo/Products/EventHandlers/ProductPriceChangedEventHandler.cs
@@ -0,0 +1,11 @@
+﻿namespace Catalogo.Products.EventHandlers;
+public class ProductPriceChangedEventHandler(ILogger<ProductPriceChangedEventHandler> logger)
+    : INotificationHandler<ProductPriceChangedEvent>
+{
+    public Task Handle(ProductPriceChangedEvent notification, CancellationToken cancellationToken)
+    {
+        // TODO: publish product price changed integration event for update basket prices(rabbit)
+        logger.LogInformation("Domain Event handled: {DomainEvent}", notification.GetType().Name);
+        return Task.CompletedTask;
+    }
+}
