4016e2d738ffa02a679de60c5ec1cd7b49e6abec
Luis Cuya <luis.cuya@intelica.com>
2025-06-17 15:56:34 -0500

    006 - Se crear los controlers(endpoints) para las api minimas usando Carter

---
+++ src/Modules/Catalogo/Catalogo/Products/Features/UpdateProduct/UpdateProductEndpoint.cs
@@ -0,0 +1,29 @@
+﻿namespace Catalogo.Products.Features.UpdateProduct;
+
+public record UpdateProductRequest(ProductDto Product);
+public record UpdateProductResponse(bool IsSuccess);
+
+public class UpdateProductEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPut("/products", async (UpdateProductRequest request, ISender sender) =>
+        {
+            //adapto la solicitud entrante del request para pasarle un tipo command que se necesita en clase handler
+            //significa que a mi dto le convertire en tipo command, pero las propiedades se mantienen tal cual esta el dto
+            var command = request.Adapt<UpdateProductCommand>();
+
+            var result = await sender.Send(command);
+
+            var response = result.Adapt<UpdateProductResponse>();
+
+            return Results.Ok(response);
+        })
+        .WithName("UpdateProduct")
+        .Produces<UpdateProductResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .ProducesProblem(StatusCodes.Status404NotFound)
+        .WithSummary("Update Product")
+        .WithDescription("Update Product");
+    }
+}
