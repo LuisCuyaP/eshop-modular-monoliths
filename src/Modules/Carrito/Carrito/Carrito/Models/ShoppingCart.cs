c997813bc506a5ac54eb42189d7c353ae08c9320
Luis Cuya <luis.cuya@intelica.com>
2025-06-24 10:45:40 -0500

    Se crea la logica del dominio en la cabecera, en el detalle no se debe aplicar logica

--- src/Modules/Carrito/Carrito/Carrito/Models/ShoppingCart.cs
+++ src/Modules/Carrito/Carrito/Carrito/Models/ShoppingCart.cs
@@ -1,6 +1,4 @@
-﻿using Shared.DDD;
-
-namespace Carrito.Carrito.Models;
+﻿namespace Carrito.Carrito.Models;
 public class ShoppingCart : Aggregate<Guid>
 {
     public string UserName { get; private set; } = default!;
@@ -8,4 +6,45 @@ public class ShoppingCart : Aggregate<Guid>
     private readonly List<ShoppingCartItem> _items = new();
     public IReadOnlyList<ShoppingCartItem> Items => _items.AsReadOnly();
     public decimal TotalPrice => Items.Sum(x => x.Price * x.Quantity);
+
+    public static ShoppingCart Create(Guid id, string userName)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(userName);
+
+        var shoppingCart = new ShoppingCart
+        {
+            Id = id,
+            UserName = userName
+        };
+
+        return shoppingCart;
+    }
+
+    public void AddItem(Guid productId, int quantity, string color, decimal price, string productName)
+    {
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(quantity);
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(price);
+
+        var existingItem = Items.FirstOrDefault(x => x.ProductId == productId);
+
+        if (existingItem != null)
+        {
+            existingItem.Quantity += quantity;
+        }
+        else
+        {
+            var newItem = new ShoppingCartItem(Id, productId, quantity, color, price, productName);
+            _items.Add(newItem);
+        }
+    }
+
+    public void RemoveItem(Guid productId)
+    {
+        var existingItem = Items.FirstOrDefault(x => x.ProductId == productId);
+
+        if (existingItem != null)
+        {
+            _items.Remove(existingItem);
+        }
+    }
 }
