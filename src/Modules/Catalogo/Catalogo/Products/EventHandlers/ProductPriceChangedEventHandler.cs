daa4b3e0acf88bca5c7811bf5a8fcc8c95a111b6
Luis Cuya <luis.cuya@intelica.com>
2025-07-01 12:05:20 -0500

    013 - Se crea la publicacion de evento de integracion para precioModificado en catalogo/product/eventhandlers/ProductPriceChangedEventHandler, el cual este handler es un evento de dominio pero que dentro crea un evento de integracion y lo publica al rabbitMQ

--- src/Modules/Catalogo/Catalogo/Products/EventHandlers/ProductPriceChangedEventHandler.cs
+++ src/Modules/Catalogo/Catalogo/Products/EventHandlers/ProductPriceChangedEventHandler.cs
@@ -1,11 +1,25 @@
-﻿namespace Catalogo.Products.EventHandlers;
-public class ProductPriceChangedEventHandler(ILogger<ProductPriceChangedEventHandler> logger)
+﻿using MassTransit;
+using Shared.Messaging.Events;
+
+namespace Catalogo.Products.EventHandlers;
+public class ProductPriceChangedEventHandler(IBus bus, ILogger<ProductPriceChangedEventHandler> logger)
     : INotificationHandler<ProductPriceChangedEvent>
 {
-    public Task Handle(ProductPriceChangedEvent notification, CancellationToken cancellationToken)
+    public async Task Handle(ProductPriceChangedEvent notification, CancellationToken cancellationToken)
     {
-        // TODO: publish product price changed integration event for update basket prices(rabbit)
         logger.LogInformation("Domain Event handled: {DomainEvent}", notification.GetType().Name);
-        return Task.CompletedTask;
+
+        // TODO: publish product price changed integration event for update basket prices(rabbit)
+        var integrationEvent = new ProductPriceChangedIntegrationEvent
+        {
+            ProductId = notification.Product.Id,
+            Name = notification.Product.Name,
+            Category = notification.Product.Category,
+            Description = notification.Product.Description,
+            ImageFile = notification.Product.ImageFile,
+            Price = notification.Product.Price //actualizar el precio del producto
+        };
+        
+        await bus.Publish(integrationEvent, cancellationToken);
     }
 }
