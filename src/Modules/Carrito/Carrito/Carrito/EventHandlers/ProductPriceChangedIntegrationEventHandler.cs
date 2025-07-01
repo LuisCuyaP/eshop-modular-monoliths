22fe2394ea21f6ab89c239c6f1e82c88f9596c6f
Luis Cuya <luis.cuya@intelica.com>
2025-07-01 12:23:40 -0500

    013 - Se crea el evento de integracion(carrito/carrito/eventhandlers/ProductPriceChangedIntegrationEventHandler) que se subscribira y estara escuchando atentamente al evento de integracion que se publico en catalogo/product/eventhandlers/ProductPriceChangedEventHandler

---
+++ src/Modules/Carrito/Carrito/Carrito/EventHandlers/ProductPriceChangedIntegrationEventHandler.cs
@@ -0,0 +1,14 @@
+using MassTransit;
+using Microsoft.Extensions.Logging;
+using Shared.Messaging.Events;
+
+namespace Carrito.Carrito.EventHandlers;
+public class ProductPriceChangedIntegrationEventHandler(ISender sender, ILogger<ProductPriceChangedIntegrationEventHandler> logger) : IConsumer<ProductPriceChangedIntegrationEvent>
+{
+    public Task Consume(ConsumeContext<ProductPriceChangedIntegrationEvent> context)
+    {        
+        logger.LogInformation("Integration Event handled: {IntegrationEvent}", context.Message.GetType().Name);
+
+        return Task.CompletedTask;     
+    }
+}
